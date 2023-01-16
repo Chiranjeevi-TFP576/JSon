0fe3e24984ce0fb019258d10788e9dace01a507d
Chiranjeevi-TFP576 <p.chiranjeevi12345@gmail.com>
2023-01-16 19:26:19 +0530

    [chiru] Inventory Of Items

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using Newtonsoft.Json;
+using Json_Demo;
+
+namespace Json_Demo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            JsonInventoryMain Json = new JsonInventoryMain();
+            Json.Convert("D:\\Batch 244\\Json_Demo\\Inventory.json");
+        }
+    }
+}
